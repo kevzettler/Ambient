f14373248673747ed28056c7ec81f59feb185778
Kev Zettler <kevzettler@gmail.com>
2024-02-04 11:48:37 -0800

    fix vite web build

--- web/www/vite.config.js
+++ web/www/vite.config.js
@@ -1,16 +1,23 @@
 import wasm from "vite-plugin-wasm";
 import { defineConfig } from "vite";
-
-// import topLevelAwait from "vite-plugin-top-level-await";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-    plugins: [wasm()],
-    server: {
-        headers: {
-            // Enables shared array buffers
-            "Access-Control-Allow-Origin": "*",
-            "Cross-Origin-Opener-Policy": "same-origin",
-            "Cross-Origin-Embedder-Policy": "require-corp",
-        },
+  plugins: [
+    wasm(),
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: "__tla",
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: i => `__tla_${i}`,
+    })
+  ],
+  server: {
+    headers: {
+      // Enables shared array buffers
+      "Access-Control-Allow-Origin": "*",
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
+  },
 });
